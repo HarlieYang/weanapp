d8b567ec3b36425f724894c2b407f9a916d51fa6
杨惠 <yanghui3021@163..com>
2020-10-19 00:24:18 +0800

    底部导航

---
+++ pages/index/index.js
@@ -0,0 +1,39 @@
+//index.js
+//获取应用实例
+import {jumpTo} from '../../utils/util'
+const app = getApp()
+
+Page({
+  data: {
+    weanText: [
+      {
+        name: '业务组件',
+        textData:[
+          {
+            name: '底部导航栏',
+            url: '/pages/tabbar/index'
+          },
+          {
+            name: '滚动导航条',
+            url: '/pages/tabbar/index'
+          },
+          {
+            name: '抽屉',
+            url: '/pages/tabbar/index'
+          },
+        ]
+      },
+      {
+        name: '营销组件',
+        textData:[]
+      }
+    ]
+  },
+  onLoad: function () {
+    
+  },
+  toUrl(e){
+    let {url} = e.target.dataset.item
+    jumpTo(url)
+  }
+})
